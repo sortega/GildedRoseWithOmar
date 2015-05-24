93cf6f4a971d5a469427ccf3ab23aad99ed6d62f
Sebastian Ortega <sortega@coinffeine.com>
2015-05-24 12:43:10 +0200

    Add a generative regression test

--- src/main/scala/com/gildedrose/Item.scala
+++ src/main/scala/com/gildedrose/Item.scala
@@ -2,4 +2,11 @@ package com.gildedrose
 
 class Item(val name: String, var sellIn: Int, var quality: Int) {
 
+  override def equals(obj: scala.Any): Boolean = obj match {
+    case other: Item =>
+      this.name == other.name && this.sellIn == other.sellIn && this.quality == other.quality
+    case _ => false
+  }
+
+  override def toString: String = s"Item($name, $sellIn, $quality)"
 }
--- src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -1,12 +1,39 @@
 package com.gildedrose
 
-import org.scalatest._
+import org.scalacheck.Gen
+import org.scalatest.prop.PropertyChecks
+import org.scalatest.{FlatSpec, ShouldMatchers}
 
-class GildedRoseTest  extends FlatSpec with Matchers {
-      it should "foo" in {
-        var items = Array[Item](new Item("foo", 0, 0))
-        val app = new GildedRose(items)
-        app.updateQuality()
-        (app.items(0).name) should equal ("fixme")
+class GildedRoseTest extends FlatSpec with ShouldMatchers with PropertyChecks {
+
+  val sellInDays = Gen.chooseNum[Int](-10, 10)
+  val qualities = Gen.posNum[Int]
+  val itemNames = Gen.oneOf(
+    "Normal item",
+    "Aged Brie",
+    "Sulfuras, Hand of Ragnaros",
+    "Backstage passes to a TAFKAL80ETC concert",
+    "+5 Dexterity Vest"
+  )
+  val items = for {
+    name <- itemNames
+    sellIn <- sellInDays
+    quality <- qualities
+  } yield new Item(name, sellIn, quality)
+  val itemArrays = Gen.containerOf[Array, Item](items)
+  val daysToPass = Gen.posNum[Int]
+
+  "The refactored gilded rose" should "behave like the golden master" in {
+    forAll(daysToPass, itemArrays) { (days, items) =>
+      val master = new GoldenMaster(items)
+      val instance = new GildedRose(items.map(copyItem))
+      for (_ <- 1 to days) {
+        master.updateQuality()
+        instance.updateQuality()
       }
+      instance.items.toList shouldBe master.items.toList
+    }
+  }
+
+  def copyItem(item: Item) = new Item(item.name, item.sellIn, item.quality)
 }
